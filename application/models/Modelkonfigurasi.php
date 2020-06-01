4f6073d768a83da0b614f854460f41a90f60a638
stmikjayanusa <rahmatnur845@gmail.com>
2020-06-01 17:30:59 +0700

    Konfigurasi&Jatah Ip Update p6

--- application/models/Modelkonfigurasi.php
+++ application/models/Modelkonfigurasi.php
@@ -1,6 +1,7 @@
 <?php 
 class Modelkonfigurasi extends CI_Model{
 	protected $tabel='konfigurasi';
+	protected $tabe2='jatah_ip';
 public function view()
 {
 		return $this->db->query("SELECT*FROM ".$this->tabel)->result_array();
@@ -16,8 +17,28 @@ public function view()
 			'rangemin'   => $params['rangemin'],
 			'rangemax'   => $params['rangemax']
 		];
+
+			$ipnetwork_left=substr($params['network'], 0, -1); //didapat 192.168.1.xxx
+			
+			$a_len=strlen($ipnetwork_left);
+			$ip_right=substr($params['ip_server'], $a_len);
+			$ip_right+=1;
+
+			$this->db->query("DELETE FROM `jatah_ip`");
+			for ($i=0; $i <= $params['jumlah_host'] ; $i++) { 
+				$ip_right+=$i;
+				$ip=$ipnetwork_left.$ip_right;
+				$this->save_jatahip($no,$ip);
+			}
 		return $this->db->insert($this->tabel, $data);
 	}
-
+public function save_jatahip($no,$ip)
+	{
+		$data = [
+			'no'   => $no,
+			'ip_address'   => $ip
+		];
+		return $this->db->insert($this->tabe2, $data);
+	}
 }
 ?>
