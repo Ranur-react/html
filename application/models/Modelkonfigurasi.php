f26c961eeab88ef0ba10963a72d40fdc30795c4e
stmikjayanusa <rahmatnur845@gmail.com>
2020-08-16 23:24:03 +0700

    Upload databasex

--- application/models/Modelkonfigurasi.php
+++ application/models/Modelkonfigurasi.php
@@ -12,35 +12,51 @@ public function view()
 			$rangemin="";
 			$rangemax="";
 			$index[0]="";
-			$ipnetwork_left=substr($params['network'], 0, -1); //didapat 192.168.1.xxx
-			$a_len=strlen($ipnetwork_left);
-			$ip_right=substr($params['ip_server_otg'], $a_len);
+// -----------------------------Koding Lama-------------------------------------
+
+
+			// $ipnetwork_left=substr($params['network'], 0, -1); //didapat 192.168.1.xxx
+			// $a_len=strlen($ipnetwork_left);
+			// $ip_right=substr($params['ip_server_otg'], $a_len);
+
+// ------------------------------------------------------------------
+
+
 			$this->db->query("DELETE FROM `jatah_ip`");
-			for ($i=0; $i < $params['jumlah_host'] ; $i++) { 
+// -----------------------------Koding Lama-------------------------------------
+
+			// for ($i=0; $i < $params['jumlah_host'] ; $i++) { 
+			// 	$ip_right+=1;
+			// 	$ip=$ipnetwork_left.$ip_right;
+			// 	$index[$i]=$ip;
+			// 	$this->save_jatahip($no=$i,$ip);
+			// }
+// -----------------------------Koding Baru-------------------------------------
+			$ip_right=$params['net_kanan'];
+			$ipnetwork_left=$params['net_kiri'];
+for ($i=0; $i < $params['jumlah_host'] ; $i++) { 
 				$ip_right+=1;
 				$ip=$ipnetwork_left.$ip_right;
 				$index[$i]=$ip;
 				$this->save_jatahip($no=$i,$ip);
 			}
-				// if ($i=0) 
-				// 		{$rangemin=$ip;
-				// 		}
-				// 	if($i=$params['jumlah_host'])
-				// 	{
-				// 			$rangemax=$ip;
-				// 	}
+// ------------------------------------------------------------------
+
+
 			$rangemin=$index[0];		
 			$nx=count($index)-1;		
 			$rangemax=$index[$nx];		
 
 				$konfig=$this->Konfigurasi();
+// ------------------------------------------------------------------
 
-					ini_set('display_errors', 1);
-					ini_set('display_startup_errors', 1);
-					error_reporting(E_ALL);
-					$connection = ssh2_connect($konfig['ip_mikrotik'], $konfig['port']);
-					ssh2_auth_password($connection, 'admin', '');	
+					// ini_set('display_errors', 1);
+					// ini_set('display_startup_errors', 1);
+					// error_reporting(E_ALL);
+					// $connection = ssh2_connect($konfig['ip_mikrotik'], $konfig['port']);
+					// ssh2_auth_password($connection, 'admin', '');	
 					 
+// ------------------------------------------------------------------
 
 						 $data = [
 									'ip_mikrotik'   => $params['ip_mikrotik'],
@@ -58,42 +74,58 @@ public function view()
 
 							// 		ip dhcp-server enable hotspotBrdg
 //add and remove address interface
+// ------------------------------------------------------------------
 
 
-					$stream = ssh2_exec($connection, 'ip address add address='.$params['ip_server'].'/24'.' network='.$params['network'].' interface=hotspotBrdg');
+					// $stream = ssh2_exec($connection, 'ip address add address='.$params['ip_server'].'/24'.' network='.$params['network'].' interface=hotspotBrdg');
+// ------------------------------------------------------------------
+
 //end---add and remove address interface
 // ip pool 
-					$stream = ssh2_exec($connection, 'ip dhcp-server network remove 0');
-					$stream = ssh2_exec($connection, 'ip dhcp-server remove hotspotBrdg');
-					$stream = ssh2_exec($connection, 'ip pool remove hotspotBrdg');
+// ------------------------------------------------------------------
+
+					// $stream = ssh2_exec($connection, 'ip dhcp-server network remove 0');
+					// $stream = ssh2_exec($connection, 'ip dhcp-server remove hotspotBrdg');
+					// $stream = ssh2_exec($connection, 'ip pool remove hotspotBrdg');
 
 				
 
 
-					$stream = ssh2_exec($connection, 'ip pool add name=hotspotBrdg ranges='.$rangemin.'-'.$rangemax);
+					// $stream = ssh2_exec($connection, 'ip pool add name=hotspotBrdg ranges='.$rangemin.'-'.$rangemax);
+// ------------------------------------------------------------------
+
 //end pool range
 
 //dhcp 
 
 
 
+// ------------------------------------------------------------------
 	
-					$stream = ssh2_exec($connection, 'ip dhcp-server network add address='.$params['network'].'/24'.' gateway='.$params['ip_server'].' dns-server=8.8.8.8');
+					// $stream = ssh2_exec($connection, 'ip dhcp-server network add address='.$params['network'].'/24'.' gateway='.$params['ip_server'].' dns-server=8.8.8.8');
+// ------------------------------------------------------------------
+
 
 					//end dhcp
+// ------------------------------------------------------------------
 
-					$stream = ssh2_exec($connection,'ip dhcp-server add name=hotspotBrdg address-pool=hotspotBrdg interface=hotspotBrdg');
 
-					$stream = ssh2_exec($connection,'ip dhcp-server enable hotspotBrdg');
-					$stream = ssh2_exec($connection,'ip hotspot remove hotspotsmk');
-					$stream = ssh2_exec($connection,'ip hotspot add name=hotspotsmk interface=hotspotBrdg address-pool=hotspotBrdg profile=wifi');
-					$stream = ssh2_exec($connection,'ip hotspot enable hotspotsmk');
+					// $stream = ssh2_exec($connection,'ip dhcp-server add name=hotspotBrdg address-pool=hotspotBrdg interface=hotspotBrdg');
+
+					// $stream = ssh2_exec($connection,'ip dhcp-server enable hotspotBrdg');
+					// $stream = ssh2_exec($connection,'ip hotspot remove hotspotsmk');
+					// $stream = ssh2_exec($connection,'ip hotspot add name=hotspotsmk interface=hotspotBrdg address-pool=hotspotBrdg profile=wifi');
+					// $stream = ssh2_exec($connection,'ip hotspot enable hotspotsmk');
  
+// ------------------------------------------------------------------
 
-					if ($stream) {
-						# code...
+					// if ($stream) {
+					// 	# code...
+					// 	return $this->db->insert($this->tabel, $data);
+					// }
+// ------------------------------------------------------------------
 						return $this->db->insert($this->tabel, $data);
-					}
+					
 
 	}
 public function save_jatahip($no,$ip)
