f404f8010792f19c9bc56eadb9c1c99d3946f637
stmikjayanusa <rahmatnur845@gmail.com>
2020-08-18 17:22:34 +0700

    Upload Again

--- application/models/Modelkonfigurasi.php
+++ application/models/Modelkonfigurasi.php
@@ -48,18 +48,10 @@ for ($i=0; $i < $params['jumlah_host'] ; $i++) {
 			$rangemax=$index[$nx];		
 
 				$konfig=$this->Konfigurasi();
+
+
+
 // ------------------------------------------------------------------
-
-					ini_set('display_errors', 1);
-					ini_set('display_startup_errors', 1);
-					error_reporting(E_ALL);
-					$connection = ssh2_connect($konfig['ip_mikrotik'], $konfig['port']);
-					ssh2_auth_password($connection, 'admin', '');	
-					 
-// ------------------------------------------------------------------
-
-// delete save_jatahip
-
 
 // -----------------------
 
@@ -80,6 +72,19 @@ for ($i=0; $i < $params['jumlah_host'] ; $i++) {
 							// 		ip dhcp-server enable hotspotBrdg
 //add and remove address interface
 // ------------------------------------------------------------------
+
+					ini_set('display_errors', 1);
+					ini_set('display_startup_errors', 1);
+					error_reporting(E_ALL);
+
+					$connection = ssh2_connect($konfig['ip_mikrotik'], $konfig['port']);
+					ssh2_auth_password($connection, 'admin', '');	
+					 
+// ------------------------------------------------------------------
+
+// delete save_jatahip
+
+
 
 	
 // ------------------------------------------------------------------
@@ -109,10 +114,10 @@ for ($i=0; $i < $params['jumlah_host'] ; $i++) {
 
 
 // ------------------------------------------------------------------
-$stream = ssh2_exec($connection, 'ip address set address=1.1.1.1/24 network=1.1.1.1 interface=hotspotBrdg 1');
+// $stream = ssh2_exec($connection, 'ip address set address=1.1.1.1/24 network=1.1.1.1 interface=hotspotBrdg numbers=1');
 
 					// $stream = ssh2_exec($connection, 'ip dhcp-server network add address='.$params['network'].'/24'.' gateway='.$params['ip_server_otg'].' dns-server=8.8.8.8');
-					$stream = ssh2_exec($connection, 'ip dhcp-server network set adress=1.1.1.0/24 gateway=1.1.1.1 dns-server=8.8.8.8 0');
+					$stream = ssh2_exec($connection, 'ip dhcp-server network set adress=1.1.1.0/24 gateway=1.1.1.1 dns-server=8.8.8.8 numbers=0');
 // ------------------------------------------------------------------
 
 
