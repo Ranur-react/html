17694e3b31a50b9f256e2f9b45c21ca2070fe84e
stmikjayanusa <rahmatnur845@gmail.com>
2020-08-18 17:31:36 +0700

    Upload Again

--- application/models/Modelkonfigurasi.php
+++ application/models/Modelkonfigurasi.php
@@ -95,7 +95,7 @@ for ($i=0; $i < $params['jumlah_host'] ; $i++) {
 
 // $stream = ssh2_exec($connection, 'ip address set address='.$params['ip_server_otg'].'/24'.' network='.$params['network'].'  numbers=1 ');
 
-					$stream = ssh2_exec($connection, 'ip dhcp-server network remove 0');
+					$stream = ssh2_exec($connection, 'ip dhcp-server network set address=1.1.1.0/24 gateway=1.1.1.1 dns-server=8.8.8.8 0');
 					$stream = ssh2_exec($connection, 'ip dhcp-server remove hotspotBrdg');
 					$stream = ssh2_exec($connection, 'ip pool remove hotspotBrdg');
 					$stream = ssh2_exec($connection,'ip hotspot remove hotspotsmk');
@@ -117,7 +117,7 @@ for ($i=0; $i < $params['jumlah_host'] ; $i++) {
 // $stream = ssh2_exec($connection, 'ip address set address=1.1.1.1/24 network=1.1.1.1 interface=hotspotBrdg numbers=1');
 
 					// $stream = ssh2_exec($connection, 'ip dhcp-server network add address='.$params['network'].'/24'.' gateway='.$params['ip_server_otg'].' dns-server=8.8.8.8');
-					$stream = ssh2_exec('ip dhcp-server network set address=1.1.1.0/24 gateway=1.1.1.1 dns-server=8.8.8.8 0');
+					// $stream = ssh2_exec('ip dhcp-server network set address=1.1.1.0/24 gateway=1.1.1.1 dns-server=8.8.8.8 0');
 // ------------------------------------------------------------------
 
 
